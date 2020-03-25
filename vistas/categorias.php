6334750b2d39b4afb3ac23f68bcbf6b1f68e8104
kiritosan <dcmmsis4710@gmail.com>
2020-03-24 23:20:32 -0400

    Sistema Gestor de Archivos - Maquetación y sql del modulo categorías (parte 2)

--- vistas/categorias.php
+++ vistas/categorias.php
@@ -10,14 +10,54 @@ if (isset($_SESSION['usuario'])) {
         <div class="container">
             <h1 class="display-4">Categorías</h1>
             <div class="row">
+                <div class="col-sm-4">
+                    <span class="btn btn-primary" data-toggle="modal" data-target="#modalAgregaCatergoria">
+                        <span class="fas fa-plus-circle"></span>Agregar Nueva Categoria
+                    </span>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
                 <div class="col-sm-12">
                     <div id="tablaCategorias"></div>
                 </div>
             </div>
         </div>
     </div>
-<?php
+
+    <!-- Modal -->
+    <div class="modal fade" id="modalAgregaCatergoria" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Agregar Nueva Categoria</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="" id="frmCategorias">
+                        <label for="" >Nombre de la Categoría</label>
+                        <input type="text" name="nombreCategoria" id="nombreCategoria" class="form-control">
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-primary">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
     include "footer.php";
+    ?>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#tablaCategorias').load("categorias/tablaCategoria.php");
+        })
+    </script>
+
+<?php
 } else {
     header("location:../index.html");
 }
---
+++ vistas/categorias/tablaCategoria.php
@@ -0,0 +1,28 @@
+<div class="table-responsive">
+    <table class="table table-hover table-dark">
+        <thead>
+            <tr style="text-align: center">
+                <th>Nombre</th>
+                <th>Fecha</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td style="text-align: center">
+                    <span class="btn btn-warning btn-sm">
+                        <span class="fas fa-edit"></span>
+                    </span>
+                </td>
+                <td style="text-align: center">
+                    <span class="btn btn-danger btn-sm">
+                        <spanc class="fas fa-trash-alt"></spanc>
+                    </span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
