d3b2f71ca0cc269bc1b487034ad1ac55de0a0a1e
kiritosan <dcmmsis4710@gmail.com>
2020-04-06 17:01:48 -0400

    31.- Sistema Gestor de Archivos - Mostrar y descargar archivos

--- vistas/gestor/tablaGestor.php
+++ vistas/gestor/tablaGestor.php
@@ -1,24 +1,57 @@
+<?php
+
+    session_start();
+    require_once "../../clases/Conexion.php";
+    $c = new Conectar();
+    $conexion = $c->conexion();
+    $idUsuario = $_SESSION['idUsuario'];
+    
+    $sql = "SELECT 
+                    archivos.nombre AS idArchivo,
+                    usuario.nombre AS nombreUsuario,
+                    categorias.nombre AS categoria,
+                    archivos.nombre AS nombreArchivo,
+                    archivos.tipo AS tipoArchivo,
+                    archivos.ruta AS rutaArchivo,
+                    archivos.fecha AS fecha
+                FROM
+                    t_archivos AS archivos
+                        INNER JOIN
+                    t_usuarios AS usuario ON archivos.id_usuario = usuario.id_usuario
+                        INNER JOIN
+                    t_categorias AS categorias ON archivos.id_categoria = categorias.id_categoria
+                        AND archivos.id_usuario = '$idUsuario'";
+    $result = mysqli_query($conexion, $sql);
+
+?>
+
 <div class="col-sm-12">
     <div class="table-responsive">
         <table class="table table-hover table-dark" id="tablaGestorDataTable">
             <thead>
                 <tr>
                     <th>Nombre Convenio</th>
-                    <th>Obejtivo</th>
-                    <th>Fecha Suscripcion</th>
-                    <th>Vigencia</th>
+                    <th>Tipo de Archivo</th>
                     <th>Descargar</th>
-                    <th>Visulalizar</th>
+                    <th>Visulalizar</th>                    
                     <th>Eliminar</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                <?php
+                    while($mostrar = mysqli_fetch_array($result)){
+                        
+                        $rutaDescarga = "../archivos/".$idUsuario."/".$mostrar['nombreArchivo'];
+                        $nombreArchivo = $mostrar['nombreArchivo'];
+                ?>
+                <tr>                    
+                    <td><?php echo $mostrar['nombreArchivo'];  ?></td>
+                    <td><?php echo $mostrar['tipoArchivo'];  ?></td>
+                    <td>
+                        <a href="<?php echo $rutaDescarga; ?>" download="<?php echo $nombreArchivo; ?>" class="btn btn-success btn-sm">
+                            <span class="fas fa-download"></span>
+                        </a>
+                    </td>
                     <td></td>
                     <td>
                         <span class="btn btn-danger btn-sm">
@@ -26,6 +59,9 @@
                         </span>
                     </td>
                 </tr>
+                <?php
+                    }
+                ?>
             </tbody>
         </table>
     </div>
